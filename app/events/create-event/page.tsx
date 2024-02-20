ea1390172683471c1f1b0e7624779b6b95c4adab
Daniel <da_919@hotmail.com>
2024-02-20 10:40:55 -0500

    se protege la ruta de la pagina

--- app/events/create-event/page.tsx
+++ app/events/create-event/page.tsx
@@ -1,14 +1,19 @@
-import { getSession } from "@auth0/nextjs-auth0";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import EventForm from "@components/app/components/EventForm";
 import { Auth0User } from "@components/app/lib/definitions";
+import { NextPage } from "next";
 
-type Props = {
-};
-export default async function page({}: Props) {
-  const { user } = (await getSession()) as { user: Auth0User };
-  return (
-    <div className='min-h-screen'>
-      <EventForm user={user.nickname} />
-    </div>
-  );
-}
+type Props = {};
+
+const page: NextPage = withPageAuthRequired(
+  async ({}: Props) => {
+    const { user } = (await getSession()) as { user: Auth0User };
+    return (
+      <div className='min-h-screen'>
+        <EventForm user={user.nickname} />
+      </div>
+    );
+  },
+  { returnTo: "/events/create-event" }
+);
+export default page;
