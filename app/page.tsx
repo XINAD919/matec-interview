95cda323408516645350b722c087528294ecf234
Daniel <da_919@hotmail.com>
2024-02-20 10:42:15 -0500

    se cambia el titulo

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-slate-900'>
-      <h1 className='text-4xl uppercase font-bold'>
-        Registra tus eventos aquí
+    <main className='flex min-h-screen flex-col items-center justify-enter p-24 bg-slate-900'>
+      <h1 className='text-4xl uppercase font-bold text-center'>
+        Tu pagina de eventos favorita XD
       </h1>
     </main>
   );
