f99fd3f31f722967c2677b69b6f3e126e8157990
Daniel <da_919@hotmail.com>
2024-02-20 00:18:03 -0500

    se agregan los dominios de picsum y unsplash a image

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "s.gravatar.com",
       },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };
